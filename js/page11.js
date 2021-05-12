fca5e7c625798567bf0ea80da089b87657dd3c79
Victor Chiong <cjvicro@gmail.com>
2021-05-12 22:28:41 +0800

    completed page 10, saving every information the user changes to session storage

--- js/page11.js
+++ js/page11.js
@@ -74,7 +74,19 @@ downQuantity.forEach((elem, index) => {
     });
 });
 
-
+let navigationLinksProductText = document.querySelectorAll(".navigation__links span")[2];
+let colorTextNavigationLink;
+let colorselected = (sessionStorage.getItem("item-image") === null)? -1: sessionStorage.getItem("color");
+if(colorselected!= -1){
+    switch(parseInt(sessionStorage.getItem("item-index"))){
+        case 0: colorTextNavigationLink = "Pink";break; 
+        case 1: colorTextNavigationLink = "Red";break; 
+        case 2: colorTextNavigationLink = "Black";break; 
+        case 3: colorTextNavigationLink = "White";break; 
+        case 4: colorTextNavigationLink = "Brown";break; 
+    }
+    navigationLinksProductText.innerText = `Beat Solo2 on Ear Headphones - ${colorTextNavigationLink}`;
+}
 
 
 let itemDelete = document.querySelectorAll('.items-list__item--name__delete');
